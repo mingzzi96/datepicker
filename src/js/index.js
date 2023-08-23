7bda0410a239457348bb33f96613e0b8fd63aed6
mingming <jimin23623@gmail.com>
2023-08-23 14:21:51 +0900

    캘린더 날짜 클릭 토글 애니메이션까지 추가 완료

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,79 @@
-console.log("hi")
+class DatePicker {
+  monthData = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  #calendarDate = {
+    data: "",
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  selectedDate = {
+    data: "",
+    date: 0,
+    month: 0,
+    year: 0,
+  };
+
+  datePickerEl;
+  dateInputEl;
+  calendarEl;
+  calendarMonthEl;
+  monthContentEl;
+  nextBtnEl;
+  prevBtnEl;
+  calendarDatesEl;
+
+  constructor() {
+    this.assignElement();
+    this.addEvent();
+  }
+
+  initCalendarDate() {
+    // 현재 기준의 날짜 정보를 가져와서 초기화해 준다.
+    const data = new Date();
+    const date = data.getDate();
+    const month = data.getMonth(); // index처럼 0 부터 출력하니 주의
+    const year = data.getFullYear();
+    this.#calendarDate = {
+      data,
+      date,
+      month,
+      year,
+    };
+  }
+
+  assignElement() {
+    this.datePickerEl = document.getElementById("date-picker");
+    this.dateInputEl = this.datePickerEl.querySelector("#date-input");
+    this.calendarEl = this.datePickerEl.querySelector("#calendar");
+    this.calendarMonthEl = this.calendarEl.querySelector("#month");
+    this.monthContentEl = this.calendarMonthEl.querySelector("#content");
+    this.nextBtnEl = this.calendarMonthEl.querySelector("#next");
+    this.prevBtnEl = this.calendarMonthEl.querySelector("#prev");
+    this.calendarDatesEl = this.calendarEl.querySelector("#dates");
+  }
+
+  addEvent() {
+    this.dateInputEl.addEventListener("click", this.toggleCalendar.bind(this));
+  }
+
+  toggleCalendar() {
+    this.calendarEl.classList.toggle("active");
+  }
+}
+
+new DatePicker();
